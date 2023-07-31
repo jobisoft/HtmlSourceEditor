6e37150ce5698c1cf45e0a193a548f593008b3c4
John Bieling <john.bieling@gmx.de>
2023-07-31 12:06:06 +0200

    adjust stationary overlay code to work with ESR

--- api/EditHTML/implementation.js
+++ api/EditHTML/implementation.js
@@ -1,29 +1,63 @@
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
+var { ExtensionParent } = ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
 
 function install(composeWindow) {
-  
-  let stationeryXul = `
-  <vbox id="appcontent">
-  <hbox id="stationery-tabbox-box" insertbefore="content-frame">
-    <tabbox id="stationery-content-tab" collapsed="true">
-      <tabs onselect="Stationery_.SelectEditMode(this.selectedIndex, false);" flex="1">
-        <tab label="&stationery.Composer.Tab.Edit;" onfocus="document.getElementById('content-frame').contentWindow.focus()" />
-        <tab label="&stationery.Composer.Tab.Source;" onfocus="Stationery.sourceEditor.focus(window)" />
+  console.log("INSTALL");
+
+  composeWindow.EditHTMLAddon = {
+    FocusSourceCodeEditor: function () {
+      console.log("FocusSourceCodeEditor")
+      let messageEditor = composeWindow.document.getElementById("messageEditor");
+      let htmleditor = composeWindow.document.getElementById("edithtmladdon-content-source-ace");
+      messageEditor.collapsed = true;
+      htmleditor.collapsed = false;
+
+    },
+    FocusDefaultEditor: function () {
+      console.log("FocusDefaultEditor");
+      let messageEditor = composeWindow.document.getElementById("messageEditor");
+      let htmleditor = composeWindow.document.getElementById("edithtmladdon-content-source-ace");
+      messageEditor.collapsed = false;
+      htmleditor.collapsed = true;
+      composeWindow.document.getElementById('messageEditor').contentWindow.focus()
+    }
+  }
+
+  let extension = ExtensionParent.GlobalManager.getExtension("edithtmlsource@jobisoft.de");
+
+  let tabBox = `
+  <hbox id="edithtmladdon-tabbox-box">
+    <tabbox id="edithtmladdon-content-tabbox">
+      <tabs id="edithtmladdon-content-tabs" flex="1">
+        <tab id="edithtmladdon-content-tab1" label="${extension.localeData.localizeMessage("stationery.Composer.Tab.Edit")}" />
+        <tab id="edithtmladdon-content-tab2" label="${extension.localeData.localizeMessage("stationery.Composer.Tab.Source")}" />
       </tabs> 
     </tabbox>
-  </hbox>
-  
-  <vbox id="stationery-content-source-box" flex="1" collapsed="true">
-    <iframe id="stationery-content-source-ace" data-preview="true" flex="1" src="chrome://stationery/content/html-source-editor.html" />
-  </vbox>
-  </vbox>`;
-  
-  console.log("PATCHING");
+    <vbox id="edithtmladdon-content-source-box" flex="1" collapsed="true">
+      <iframe id="edithtmladdon-content-source-ace" data-preview="true" flex="1" src="chrome://stationery/content/html-source-editor.html" />
+    </vbox>
+  </hbox>`;
+  let tabBoxElement = Array.from(composeWindow.MozXULElement.parseXULToFragment(tabBox).children).shift();
+  let messageArea = composeWindow.document.getElementById("messageArea");
+  let messageEditor = composeWindow.document.getElementById("messageEditor");
+  messageArea.insertBefore(tabBoxElement, messageEditor);
+
+  let tab1 = composeWindow.document.getElementById("edithtmladdon-content-tab1");
+  let tab2 = composeWindow.document.getElementById("edithtmladdon-content-tab2");
+  tab1.addEventListener("click", composeWindow.EditHTMLAddon.FocusDefaultEditor)
+  tab2.addEventListener("click", composeWindow.EditHTMLAddon.FocusSourceCodeEditor)
+
+  let htmleditor = composeWindow.document.getElementById("edithtmladdon-content-source-ace");
+
 }
 
 function uninstall(composeWindow) {
-  console.log("UNPATCHING");
+  console.log("UNINSTALL");
+
+  let tabbox = composeWindow.document.getElementById("edithtmladdon-tabbox-box");
+  tabbox.remove();
+  delete composeWindow.EditHTMLAddon;
 }
 
 var EditHTML = class extends ExtensionCommon.ExtensionAPI {
