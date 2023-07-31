7156130ddf836f20a91d740d1ed069c6ca7df15d
John Bieling <john.bieling@gmx.de>
2023-07-31 12:28:48 +0200

    register resource url

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
-browser.tabs.onCreated.addListener(async tab => {
-    if (tab.type == "messageCompose") {
-        let composeDetails = await browser.compose.getComposeDetails(tab.id);
-        console.log(composeDetails);
-        await browser.EditHTML.patchComposer(tab.windowId);
-    }
-})
-
 async function init() {
+    await browser.ResourceUrl.register("edithtmladdon","chrome/");
+    
     let composeWindows = await browser.windows.getAll({windowTypes: ["messageCompose"]});
     for (let composeWindow of composeWindows) {
         await browser.EditHTML.patchComposer(composeWindow.id);
     }
+
+    browser.tabs.onCreated.addListener(async tab => {
+        if (tab.type == "messageCompose") {
+            let composeDetails = await browser.compose.getComposeDetails(tab.id);
+            console.log(composeDetails);
+            await browser.EditHTML.patchComposer(tab.windowId);
+        }
+    })
 }
 init();
