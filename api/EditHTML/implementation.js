afc41df62ae98e526fac4a9db036895340c64cee
John Bieling <john.bieling@gmx.de>
2023-07-31 11:14:04 +0200

    add original stationary XUL from \chrome\content\stationery-composer.xul

--- api/EditHTML/implementation.js
+++ api/EditHTML/implementation.js
@@ -2,6 +2,23 @@ var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCo
 var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
 
 function install(composeWindow) {
+  
+  let stationeryXul = `
+  <vbox id="appcontent">
+  <hbox id="stationery-tabbox-box" insertbefore="content-frame">
+    <tabbox id="stationery-content-tab" collapsed="true">
+      <tabs onselect="Stationery_.SelectEditMode(this.selectedIndex, false);" flex="1">
+        <tab label="&stationery.Composer.Tab.Edit;" onfocus="document.getElementById('content-frame').contentWindow.focus()" />
+        <tab label="&stationery.Composer.Tab.Source;" onfocus="Stationery.sourceEditor.focus(window)" />
+      </tabs> 
+    </tabbox>
+  </hbox>
+  
+  <vbox id="stationery-content-source-box" flex="1" collapsed="true">
+    <iframe id="stationery-content-source-ace" data-preview="true" flex="1" src="chrome://stationery/content/html-source-editor.html" />
+  </vbox>
+  </vbox>`;
+  
   console.log("PATCHING");
 }
 
