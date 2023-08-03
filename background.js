b5ed641bc31c2d0ca895e015c6eab4394c7b9e3b
John Bieling <john.bieling@gmx.de>
2023-08-03 09:24:45 +0200

    Add context menu entry to compose window

--- background.js
+++ background.js
@@ -1,10 +1,41 @@
-async function setButtonStatus(tabId) {
+let openEditors = new Map();
+
+async function setEditorStatus(tabId) {
     let composeDetails = await browser.compose.getComposeDetails(tabId);
     if (composeDetails.isPlainText) {
         browser.composeAction.disable(tabId);
+        browser.menus.update("edithtml", {enabled: false});
     } else {
         browser.composeAction.enable(tabId);
+        browser.menus.update("edithtml", {enabled: true});
     }
+}
+
+async function openHtmlEditor(tab) {
+    // Check if we have an editor for this tab already.
+    if (openEditors.has(tab.id)) {
+        browser.windows.update(openEditors.get(tab.id), {focused: true})
+        return;
+    };
+
+    let sourceEditWindow = await browser.windows.create({
+        type:"popup", 
+        url: `editor/html-source-editor.html?tabId=${tab.id}`,
+        allowScriptsToClose: true,
+    });
+    openEditors.set(tab.id, sourceEditWindow.id);
+
+    await new Promise(resolve => {
+        let listener = windowId => {
+            if (windowId != sourceEditWindow.id) {
+                return;
+            }
+            browser.windows.onRemoved.removeListener(listener);
+            openEditors.delete(tab.id);
+            resolve();
+        }
+        browser.windows.onRemoved.addListener(listener);
+    });
 }
 
 async function init() {
@@ -12,35 +43,23 @@ async function init() {
     
     let composeTabs = await browser.tabs.query({type:"messageCompose"});
     for (let composeTab of composeTabs) {
-        setButtonStatus(composeTab.id);
+        setEditorStatus(composeTab.id);
     }
 
     browser.tabs.onCreated.addListener(async tab => {
         if (tab.type == "messageCompose") {
-            setButtonStatus(tab.id);
+            setEditorStatus(tab.id);
         }
     })
 
-    browser.composeAction.onClicked.addListener(async (tab, info) => {
-        let sourceEditWindow = await browser.windows.create({
-            type:"popup", 
-            url: `editor/html-source-editor.html?tabId=${tab.id}`,
-            allowScriptsToClose: true,
-        });
+    browser.menus.create({
+        id: "edithtml",
+        contexts: ["compose_body"],
+        onclick: (info, tab) => openHtmlEditor(tab),
+        title: browser.i18n.getMessage("sourceHTML")
+    })
 
-        await new Promise(resolve => {
-            let listener = windowId => {
-                if (windowId != sourceEditWindow.id) {
-                    return;
-                }
-                browser.windows.onRemoved.removeListener(listener);
-                browser.composeAction.enable(tab.id);
-                resolve();
-            }
-            browser.composeAction.disable(tab.id);
-            browser.windows.onRemoved.addListener(listener);
-        });
-    });
+    browser.composeAction.onClicked.addListener((tab, info) => openHtmlEditor(tab));
 
 }
 init();
