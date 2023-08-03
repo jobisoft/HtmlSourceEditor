5bf500e7fa62dc705846916ad8ecae591d788328
John Bieling <john.bieling@gmx.de>
2023-08-03 18:34:47 +0200

    fix issue #1

--- background.js
+++ background.js
@@ -4,22 +4,22 @@ async function setEditorStatus(tabId) {
     let composeDetails = await browser.compose.getComposeDetails(tabId);
     if (composeDetails.isPlainText) {
         browser.composeAction.disable(tabId);
-        browser.menus.update("edithtml", {enabled: false});
+        browser.menus.update("edithtml", { enabled: false });
     } else {
         browser.composeAction.enable(tabId);
-        browser.menus.update("edithtml", {enabled: true});
+        browser.menus.update("edithtml", { enabled: true });
     }
 }
 
 async function openHtmlEditor(tab) {
     // Check if we have an editor for this tab already.
     if (openEditors.has(tab.id)) {
-        browser.windows.update(openEditors.get(tab.id), {focused: true})
+        browser.windows.update(openEditors.get(tab.id), { focused: true })
         return;
     };
 
     let sourceEditWindow = await browser.windows.create({
-        type:"popup", 
+        type: "popup",
         url: `editor/html-source-editor.html?tabId=${tab.id}`,
         allowScriptsToClose: true,
     });
@@ -39,9 +39,9 @@ async function openHtmlEditor(tab) {
 }
 
 async function init() {
-    browser.composeAction.setTitle({title: browser.i18n.getMessage("sourceHTML")});
-    
-    let composeTabs = await browser.tabs.query({type:"messageCompose"});
+    browser.composeAction.setTitle({ title: browser.i18n.getMessage("sourceHTML") });
+
+    let composeTabs = await browser.tabs.query({ type: "messageCompose" });
     for (let composeTab of composeTabs) {
         setEditorStatus(composeTab.id);
     }
@@ -52,12 +52,20 @@ async function init() {
         }
     })
 
-    browser.menus.create({
-        id: "edithtml",
-        contexts: ["compose_body"],
-        onclick: (info, tab) => openHtmlEditor(tab),
-        title: browser.i18n.getMessage("sourceHTML")
-    })
+    let info = await browser.runtime.getBrowserInfo();
+    try {
+        let major = parseInt(info.version.split(".").shift());
+        if (major > 114) {
+            browser.menus.create({
+                id: "edithtml",
+                contexts: ["compose_body"],
+                onclick: (info, tab) => openHtmlEditor(tab),
+                title: browser.i18n.getMessage("sourceHTML")
+            })
+        }
+    } catch (ex) {
+        // Uups.
+    }
 
     browser.composeAction.onClicked.addListener((tab, info) => openHtmlEditor(tab));
 
